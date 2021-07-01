b79097e9253f0f88894f7f55a31acf3e68d69a80
Akin Tekeoglu <akin.tekeoglu@gmail.com>
2021-07-01 13:52:31 +0300

    refactor: inject css and js correct places in layout

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as exphbs from 'express-handlebars';
 import { join } from 'path';
-import {AppModule} from './app.module'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,6 +12,18 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  app.engine('hbs', exphbs({
+    extname: '.hbs', helpers: {
+      // A helper function allow us to inject css and js files to correct place
+      // See views/layouts/main.hbs for details
+      // More info https://stackoverflow.com/questions/25300017/how-to-ensure-the-javascript-is-at-the-bottom-of-the-code-in-express-handlebars
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      }
+    }
+  }));
 
   await app.listen(3000);
 }
